9c2d5b4d539ef4fd88e0ac6e5fe03f7c3f45e708
xojiakbarjobirov19-star <xojiakbarjobirov19@gamil.com>
2026-09-04 19:30:30 +0500

    media-qo'shildi

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,9 +5,9 @@ function Hero() {
   return (
     <div className="glavniy-ekran">
       <div className="left-side">
-        <h1 className="big-title">gungan guli gulu t<br/>tutu</h1>
+        <h1 className="big-title">This is our<br/>Website follow us</h1>
         <p className="small-text">
-          bla bla bla bla bla bla bal bal bal bal bla blab laba lba bla
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis voluptas, modi veritatis labore unde doloremque obcaecati pariatur ut odit beatae.
         </p>
         <div className="knopki-ryadom">
           <button className="knopka-blue">Get Started</button>
